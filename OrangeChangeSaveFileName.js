0fb0dd9f4713ce8fedebac6a323fc5282b4ecad5
Pierre H. Lehnen <Hudell@users.noreply.github.com>
2015-10-22 23:45:14 -0200

    Update OrangeChangeSaveFileName.js

--- OrangeChangeSaveFileName.js
+++ OrangeChangeSaveFileName.js
@@ -1,27 +1,35 @@
 /*=============================================================================
-* Orange - Change Save File Name
-* By Hudell - www.hudell.com
-* OrangeChangeSaveFileName.js
-* Version: 1.0
-* Free for commercial and non commercial use.
-*=============================================================================
-* @plugindesc This plugin will help you change the base name of the save files
-* @author Hudell
-*
-* @param localFilePattern
-* @desc The pattern for the local file name.
-*       Include '%1' on the position
-*       where you want the number of the save file to be
-* Default: file%1.rpgsave
-* @default file%1.rpgsave
-*
-* @param webStorageKeyPattern
-* @desc The pattern for web storage key name.
-*       Include '%1' on the position
-*       where you want the number of the save file to be
-* Default: RPG File%1
-* @default RPG File%1
-*=============================================================================*/
+ * Orange - Change Save File Name
+ * By Hudell - www.hudell.com
+ * OrangeChangeSaveFileName.js
+ * Version: 1.0
+ * Free for commercial and non commercial use.
+ *=============================================================================
+ * @plugindesc This plugin will help you change the base name of the save files
+ * @author Hudell
+ *
+ * @param localFilePattern
+ * @desc The pattern for the local file name.
+ *       Include '%1' on the position
+ *       where you want the number of the save file to be
+ * Default: file%1.rpgsave
+ * @default file%1.rpgsave
+ *
+ * @param webStorageKeyPattern
+ * @desc The pattern for web storage key name.
+ *       Include '%1' on the position
+ *       where you want the number of the save file to be
+ * Default: RPG File%1
+ * @default RPG File%1
+ *
+ * @help
+ * ============================================================================
+ * Latest Version
+ * ============================================================================
+ * 
+ * Get the latest version of this script on http://link.hudell.com/savefilename
+ * 
+ *=============================================================================*/
 var Imported = Imported || {};
 var OrangeChangeSaveFileName = OrangeChangeSaveFileName || {};
 
@@ -29,11 +37,11 @@ var OrangeChangeSaveFileName = OrangeChangeSaveFileName || {};
   "use strict";
 
   $.Parameters = PluginManager.parameters('OrangeChangeSaveFileName');
-  
+
   // If a new localFilePattern was defined, alias the localFilePath method
   // from the StorageManager and use the new pattern
   if ($.Parameters["localFilePattern"] !== undefined) {
-    var saveFilePattern = $.Parameters["localFilePattern"];
+    var saveFilePattern = $.Parameters["localFilePattern"] || "file%1.rpgsave";
 
     var oldStorageManager_localFilePath = StorageManager.localFilePath;
     StorageManager.localFilePath = function(savefileId) {
@@ -49,7 +57,7 @@ var OrangeChangeSaveFileName = OrangeChangeSaveFileName || {};
   // If a new webStorageKeyPattern was defined, alias the webStorageKey method
   // from the StorageManager and use the new pattern
   if ($.Parameters["webStorageKeyPattern"] !== undefined) {
-    var webStorageKeyPattern = $.Parameters["webStorageKeyPattern"];
+    var webStorageKeyPattern = $.Parameters["webStorageKeyPattern"] || "RPG File%1";
 
     var oldStorageManager_webStorageKey = StorageManager.webStorageKey;
     StorageManager.webStorageKey = function(savefileId) {
@@ -69,7 +77,6 @@ if (Imported['MVCommons'] !== undefined) {
     name: "Hudell",
     website: "http://www.hudell.com"
   }, "2015-10-20");
-}
-else {
+} else {
   Imported["OrangeChangeSaveFileName"] = true;
 }
