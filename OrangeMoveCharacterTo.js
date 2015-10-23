70321b9dd0ecfa39fdb6aaa9b68210c19a727baa
Pierre H. Lehnen <Hudell@users.noreply.github.com>
2015-10-23 02:14:03 -0200

    Update OrangeMoveCharacterTo.js

--- OrangeMoveCharacterTo.js
+++ OrangeMoveCharacterTo.js
@@ -4,7 +4,8 @@
  * OrangeMoveCharacterTo.js
  * Version: 1.0
  * Free for commercial and non commercial use.
- *=============================================================================
+ *=============================================================================*/
+ /*:
  * @plugindesc This plugin adds a script call and a plugin command you can use
  *             to move the player or an event to a specific position
  *             
