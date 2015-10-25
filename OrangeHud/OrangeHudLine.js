4101902ebfff373cdb503cd5b8c0dc6002e8d623
Pierre H. Lehnen <Hudell@users.noreply.github.com>
2015-10-25 07:25:50 -0200

    Create OrangeHudLine.js

---
+++ OrangeHud/OrangeHudLine.js
@@ -0,0 +1,58 @@
+/*=============================================================================
+ * Orange - Line HUD
+ * By HUDell - www.hudell.com
+ * OrangeHudLine.js
+ * Version: 1.0
+ * Free for commercial and non commercial use.
+ *=============================================================================*/
+/*:
+ * @plugindesc Adds a new Variable to Orange Hud
+ * @author Hudell
+ *
+ * @param Pattern
+ * @desc The pattern of the line that will be drawn
+ * @default %1
+ *
+ * @param VariableId
+ * @desc The number of the variable that will be displayed on this line.
+ * @default 1
+ *
+ * @param X
+ * @desc The X position of the variable line inside the HUD
+ * @default 
+ *
+ * @param Y
+ * @desc The Y position of the variable line inside the HUD
+ * @default 
+ *
+ * @param FontFace
+ * @desc The font face to use. Leave empty to use the HUD default
+ * @default 
+ *
+ * @param FontSize
+ * @desc The font size to use. Leave empty to use the HUD default
+ * @default
+ *
+ * @param FontColor
+ * @desc The font color to use. Leave empty to use the HUD default
+ * @default
+ *
+ * @param FontItalic
+ * @desc Should use italic? Leave empty to use the HUD default
+ * @default
+ *
+ * @param ScriptPattern
+ * @desc A script call to be used instead of the Pattern
+ * @default 
+ *
+ * @help
+ * ============================================================================
+ * Latest Version
+ * ============================================================================
+ * 
+ * Get the latest version of this script on
+ * http://link.hudell.com/hud-line
+ * */
+
+var Imported = Imported || {};
+Imported["OrangeHudLine"] = true;
