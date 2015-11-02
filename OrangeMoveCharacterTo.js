4ab9aeb0a44ad531985fd864f0c45e66eb33f6f9
Pierre Lehnen <pierre@phenrique.com.br>
2015-11-02 14:39:36 -0200

    New plugin commands and features

--- OrangeMoveCharacterTo.js
+++ OrangeMoveCharacterTo.js
@@ -2,38 +2,38 @@
  * Orange - Move Character To
  * By Hudell - www.hudell.com
  * OrangeMoveCharacterTo.js
- * Version: 1.1
+ * Version: 1.2
  * Free for commercial and non commercial use.
  *=============================================================================*/
  /*:
  * @plugindesc This plugin adds a script call and a plugin command you can use to move the player or an event to a specific position.
- *             
  * @author Hudell
  *
+ * @param failedMovementDelay
+ * @desc How many frames should the characters wait before trying to move again after failing to move once.
+ * @default 30
+ *
  * @help
- * Plugin Command:
- *   ORANGEMOVETO character x y
+ * Plugin Commands Examples:
+ *   move event 10 to position 15 20 and face left
+ *   move player to event 5
+ *   move event 7 to player and follow
+ *   clear player path
+ *   clear event 7 path
+ *
+ * Script calls (can be used inside move routes):
+ *   this.setDestination(x, y, d);
+ *   this.setCharacterDestination(eventId, follow)
+ *
  * Example:
- *   ORANGEMOVETO 0 15 20
+ *   this.setDestination(15, 20, 2);
+ *   this.setCharacterDestination(10, true);
  *
- * Script call (can be used inside move routes):
- *   this.setDestination(x, y);
- * Example:
- *   this.setDestination(15, 20);
- *
- *
- * You can also add a direction for the character to face after walking,
- * like this:
- *
- * ORANGEMOVETO 0 15 20 left
- * or
- * this.setDestination(15, 20, 4);
- #
- # Direction numbers are (RPG Maker default):
- #  left = 4
- #  right = 6
- #  top = 8
- #  down = 2
+ * Direction numbers are the RPG Maker default:
+ *  left = 4
+ *  right = 6
+ *  top = 8
+ *  down = 2
  * 
  * ============================================================================
  * Latest Version
@@ -49,10 +49,10 @@ var OrangeMoveCharacterTo = OrangeMoveCharacterTo || {};
 (function($) {
   "use strict";
 
-  var oldGameInterpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
-  Game_Interpreter.prototype.pluginCommand = function(command, args) {
-    oldGameInterpreter_pluginCommand.call(this, command, args);
+  var parameters = PluginManager.parameters('OrangeMoveCharacterTo') || {};
+  var failedMovementDelay = Number(parameters.failedMovementDelay || 30);
 
+  Game_Interpreter.prototype.checkOldOrangeMoveToFormat = function(command, args) {
     if (command.toUpperCase() === 'ORANGEMOVETO') {
       var character = this.character(parseInt(args[0], 10));
 
@@ -84,11 +84,142 @@ var OrangeMoveCharacterTo = OrangeMoveCharacterTo || {};
       } else {
         character.clearDestination();
       }
+    }    
+  };
+
+  Game_Interpreter.prototype.checkOrangeClearPathCommand = function(command, args) {
+    if (command.toUpperCase() !== 'CLEAR') return;
+    if (args.length < 2) return;
+
+    var nextIndex = 0;
+    var eventId = undefined;
+
+    if (args[0].toUpperCase() === 'EVENT') {
+      eventId = parseInt(args[1], 10);
+      nextIndex = 2;
+    } else if (args[0].toUpperCase() === 'PLAYER') {
+      eventId = -1;
+      nextIndex = 1;
+    } else if (args[0].toUpperCase() === 'THIS' && args[1].toUpperCase() === 'EVENT') {
+      eventId = 0;
+      nextIndex = 2;
     }
+
+    if (args.length > nextIndex && args[nextIndex].toUpperCase() === 'PATH') {
+      var character = this.character(eventId);
+      if (character !== undefined && character !== null) {
+        character.clearDestination();
+      }
+    }
+  };
+
+  Game_Interpreter.prototype.checkNewOrangeMoveToFormat = function(command, args) {
+    if (command.toUpperCase() !== 'MOVE') return;
+    if (args.length <= 4) return;
+
+    var eventId = undefined;
+    var newEventId = undefined;
+    var newX = undefined;
+    var newY = undefined;
+    var nextIndex = 0;
+    var follow = false;
+    var direction = 0;
+
+    if (args[0].toUpperCase() === 'EVENT') {
+      eventId = parseInt(args[1], 10);
+      nextIndex = 2;
+    } else if (args[0].toUpperCase() === 'PLAYER') {
+      eventId = -1;
+      nextIndex = 1;
+    } else if (args[0].toUpperCase === 'THIS' && args[1].toUpperCase() === 'EVENT') {
+      eventId = 0;
+      nextIndex = 2;
+    } else {
+      return;
+    }
+
+    if (args[nextIndex].toUpperCase() !== 'TO') return;
+    nextIndex++;
+
+    if (args.length <= nextIndex) return;
+
+    if (args[nextIndex].toUpperCase() === 'EVENT') {
+      nextIndex++;
+      
+      if (args.length <= nextIndex) return;
+      
+      newEventId = parseInt(args[nextIndex], 10);
+      nextIndex++;
+    } else if (args[nextIndex].toUpperCase() === 'PLAYER') {
+      newEventId = -1;
+      nextIndex++;
+    } else if (args[nextIndex].toUpperCase() === 'POSITION') {
+      if (args.length <= nextIndex + 2) return;
+
+      newX = parseInt(args[nextIndex + 1], 10);
+      newY = parseInt(args[nextIndex + 2], 10);
+
+      nextIndex += 3;
+    }
+
+    if (args.length > nextIndex +1) {
+      if (args[nextIndex].toUpperCase() === 'AND') {
+        nextIndex++;
+
+        if (args[nextIndex].toUpperCase() === 'FOLLOW') {
+          follow = true;
+          nextIndex++;
+        } else if (args[nextIndex].toUpperCase() === 'FACE' || args[nextIndex].toUpperCase() === 'TURN') {
+          nextIndex++;
+
+          if (args.length > nextIndex) {
+            if (args[nextIndex].toUpperCase() === 'LEFT') {
+              direction = 4;
+            } else if (args[nextIndex].toUpperCase() === 'RIGHT') {
+              direction = 6;
+            } else if (args[nextIndex].toUpperCase() === 'UP') {
+              direction = 8;
+            } else if (args[nextIndex].toUpperCase() === 'DOWN') {
+              direction = 2;
+            } else {
+              direction = parseInt(args[nextIndex], 10);
+            }
+
+            nextIndex++;
+          }
+        }
+      }
+    }
+
+    var character = this.character(eventId);
+
+    if (newEventId !== undefined) {
+      var newCharacter = this.character(newEventId);
+      if (newCharacter === undefined || newCharacter === null) return;
+
+      character.setCharacterDestination(newCharacter, follow);
+    } else {
+      character.setDestination(newX, newY, direction);
+    }
+  };
+
+  var oldGameInterpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+  Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    oldGameInterpreter_pluginCommand.call(this, command, args);
+    this.checkOldOrangeMoveToFormat.call(this, command, args);
+    this.checkNewOrangeMoveToFormat.call(this, command, args);
+    this.checkOrangeClearPathCommand.call(this, command, args);
   };
 
   var oldGameCharacterBase_updateStop = Game_CharacterBase.prototype.updateStop;
   Game_CharacterBase.prototype.updateStop = function() {
+    var direction = undefined;
+
+    if (this._orangeMovementDelay !== undefined && this._orangeMovementDelay > 0) {
+      this._orangeMovementDelay--;
+      return;
+    }
+
     if (this._xDestination !== undefined && this._yDestination !== undefined) {
       if (this._xDestination == this._x && this._yDestination == this._y) {
         // If the character reached the destination, check if there's a direction to face
@@ -104,10 +235,49 @@ var OrangeMoveCharacterTo = OrangeMoveCharacterTo || {};
 
       if (this._xDestination !== undefined) {
         if (!this.isMoving()) {
-          var direction = this.findDirectionTo(this._xDestination, this._yDestination);
+          direction = this.findDirectionTo(this._xDestination, this._yDestination);
 
           if (direction > 0) {
             this.moveStraight(direction);
+            if (!this.isMovementSucceeded()) {
+              this._orangeMovementDelay = failedMovementDelay;
+            }
+            return;
+          }
+        }
+      }
+    }
+
+    if (this._destinationCharacter !== undefined) {
+      if (this._destinationCharacter._x === this._x && this._destinationCharacter._y == this._y) {
+        //If the stalker reached the character, check if it needs to keep following it
+        if (this._followCharacter !== true) {
+          this.clearDestination();
+        } else {
+          return;
+        }
+      }
+
+      if (this._destinationCharacter !== undefined) {
+        if (!this.isMoving()) {
+          direction = this.findDirectionTo(this._destinationCharacter._x, this._destinationCharacter._y);
+
+          if (direction > 0) {
+            this.moveStraight(direction);
+            if (!this.isMovementSucceeded()) {
+
+              //If failed to move, and it's not set to follow the character and distance is less than 1 tile, stop moving.
+              if (this._followCharacter !== true) {
+                var distance = Math.abs(this._x - this._destinationCharacter._x) + Math.abs(this._y - this._destinationCharacter._y);
+                if (distance <= 1) {
+                  this.clearDestination();
+                  return;
+                }                
+              }
+
+              this._orangeMovementDelay = failedMovementDelay;
+            }
+
             return;
           }
         }
@@ -120,7 +290,7 @@ var OrangeMoveCharacterTo = OrangeMoveCharacterTo || {};
   // Change the advanceMoveRouteIndex  to only advance the index when the character reach the destination.
   var oldGameCharacter_advanceMoveRouteIndex = Game_Character.prototype.advanceMoveRouteIndex;
   Game_Character.prototype.advanceMoveRouteIndex = function() {
-    if (this._xDestination === undefined && this._yDestination === undefined) {
+    if (this._xDestination === undefined && this._yDestination === undefined && this._destinationCharacter === undefined) {
       oldGameCharacter_advanceMoveRouteIndex.call(this);
     }
   };
@@ -145,10 +315,30 @@ var OrangeMoveCharacterTo = OrangeMoveCharacterTo || {};
     }
   };
 
+  Game_CharacterBase.prototype.setCharacterDestination = function(character, follow) {
+    if (follow === undefined) follow = false;
+
+    if (typeof(character) == "number") {
+      character = $gameMap._interpreter.character(character);
+    }
+
+    if (character === undefined) return;
+
+    if (follow === true) {
+      this._destinationCharacter = character;
+      this._followCharacter = true;
+    } else {
+      if (this._x != character._x || this._y != character._y || this.isMoving()) {
+        this._destinationCharacter = character;
+        this._followCharacter = false;
+      }
+    }
+  };
+
   //Updates Game_Temp.prototype.setDestination to only be executed when the player has no destination set on itself
   var oldGameTemp_setDestination = Game_Temp.prototype.setDestination;
   Game_Temp.prototype.setDestination = function(x, y) {
-    if ($gamePlayer._xDestination === undefined) {
+    if ($gamePlayer._xDestination === undefined && $gamePlayer._destinationCharacter === undefined && $gamePlayer._yDestination === undefined) {
       oldGameTemp_setDestination.call(this, x, y);
     }
   };
@@ -157,16 +347,9 @@ var OrangeMoveCharacterTo = OrangeMoveCharacterTo || {};
     this._xDestination = undefined;
     this._yDestination = undefined;
     this._dDestination = undefined;
+    this._destinationCharacter = undefined;
+    this._followCharacter = false;
   };
 })(OrangeMoveCharacterTo);
 
-// If MVCommons is imported, register the plugin with it's PluginManager.
-if (Imported['MVCommons'] !== undefined) {
-  PluginManager.register("OrangeMoveCharacterTo", "1.1", "Adds a move route script call that you can use to make a character go to a specific position", {
-    email: "plugins@hudell.com",
-    name: "Hudell",
-    website: "http://www.hudell.com"
-  }, "2015-10-22");
-} else {
-  Imported['OrangeMoveCharacterTo'] = true;
-}
+Imported['OrangeMoveCharacterTo'] = 1.2;
