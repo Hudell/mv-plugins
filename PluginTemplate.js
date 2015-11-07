df1a829e7bbbc81cf07b963483a8556c2ba40792
Pierre Lehnen <pierre@phenrique.com.br>
2015-11-07 12:06:21 -0200

    Added PluginTemplate.js

---
+++ PluginTemplate.js
@@ -0,0 +1,48 @@
+/*=============================================================================
+ * Orange - PluginName
+ * By Hudell - www.hudell.com
+ * PluginName.js
+ * Version: 1.0
+ * Free for commercial and non commercial use.
+ *=============================================================================*/
+/*:
+ * @plugindesc Plugin Description <OrangePluginName>
+ * @author Hudell
+ *
+ * @param paramName
+ * @desc Description
+ * @default 0
+ *
+ * @help
+ * ============================================================================
+ * Hudell's Plugins
+ * ============================================================================
+ * 
+ * Check out my website:
+ * http://hudell.com
+ * 
+ *=============================================================================*/
+var Imported = Imported || {};
+var Hudell = Hudell || {};
+Hudell.OrangePluginName = Hudell.OrangePluginName || {};
+
+(function($) {
+  "use strict";
+
+  var parameters = $plugins.filter(function(plugin) { return plugin.description.contains('<OrangePluginName>'); });
+  if (parameters.length === 0) {
+    throw new Error("Couldn't find Hudell's OrangePluginName parameters.");
+  }
+  $.Parameters = parameters[0].parameters;
+  $.Param = {};
+
+  // Param validation
+
+  //$.Param.paramName = Number($.Parameters.paramName || 0);
+
+  // Code goes here
+  
+})(Hudell.OrangePluginName);
+
+OrangePluginName = Hudell.OrangePluginName;
+Imported["OrangePluginName"] = 1.0;
