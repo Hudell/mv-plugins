74e07aeca253bc90bb14917940d1426e2ef0afd9
Hudell <git@hudell.com>
2017-07-31 03:30:54 +0000

    Added new functionality to OrangeGreenworks

--- OrangeGreenworks.js
+++ OrangeGreenworks.js
@@ -2,7 +2,7 @@
  * Orange - Greenworks
  * By Hudell - www.hudell.com
  * OrangeGreenworks.js
- * Version: 1.1
+ * Version: 1.2
  * Free for commercial and non commercial use.
  *=============================================================================*/
 /*:
@@ -15,7 +15,7 @@
  * ============================================================================
  * 
  * Check out my website to learn how to use this plugin:
- * http://hudell.com
+ * http://hudell.com/blog/orangegreenworks/
  * 
  *=============================================================================*/
 var Imported = Imported || {};
@@ -41,7 +41,62 @@ Hudell.OrangeGreenworks = Hudell.OrangeGreenworks || {};
     console.log('Activate achievement ', achievementName);
   };
 
+  $.getAchievement = function(achievementName) {
+    return false;
+  };
+
+  $.clearAchievement = function(achievementName) {
+    console.log('Clear achievement ', achievementName);
+  };
+
+  $.getNumberOfAchievements = function() {
+    return 0;
+  };
+
   $.isSteamRunning = function() {
+    return false;
+  };
+
+  $.activateGameOverlay = function(option) {
+  };
+
+  $.isGameOverlayEnabled = function() {
+    return false;
+  };
+
+  $.activateGameOverlayToWebPage = function(url) {
+    console.log('Open URL');
+  };
+
+  $.getDLCCount = function() {
+    return 0;
+  };
+
+  $.isDLCInstalled = function(dlcAppId) {
+    return false;
+  };
+
+  $.installDLC = function(dlcAppId) {
+  };
+
+  $.uninstallDLC = function(dlcAppId) {
+  };
+
+  $.getStatInt = function(name) {
+    return 0;
+  };
+
+  $.getStatFloat = function(name) {
+    return 0;
+  };
+
+  $.setStat = function(name, value) {
+    console.log('Change Stat', name, value);
+    return false;
+  };
+
+  $.storeStats = function() {
+    console.log('Store Stats');
     return false;
   };
 
@@ -82,12 +137,35 @@ Hudell.OrangeGreenworks = Hudell.OrangeGreenworks || {};
         return $.greenworks.isSteamRunning();
       };
 
+      $._storeStatsSuccess = function(){
+        console.log('Stored Stats Successfully', arguments);
+      };
+
+      $._storeStatsError = function(){
+        console.log('Failed to Store Stats', arguments);
+      };
+
       $._achievementSuccess = function(){
         console.log('Achievement activated', arguments);
       };
 
       $._achievementError = function(){
         console.log('Achievement activation error', arguments);
+      };
+
+      $._clearAchievementSuccess = function(){
+        console.log('Successfully Cleared Achievement', arguments);
+      };
+
+      $._clearAchievementError = function(){
+        console.log('Failed to Clear Achievement', arguments);
+      };
+
+      $._getAchievementSuccess = function(){
+      };
+
+      $._getAchievementError = function(){
+        console.log('Failed to check Achievement', arguments);
       };
 
       $.activateAchievement = function(achievementName) {
@@ -102,6 +180,151 @@ Hudell.OrangeGreenworks = Hudell.OrangeGreenworks || {};
         }
         
         $.greenworks.activateAchievement(achievementName, $._achievementSuccess, $._achievementError);
+      };
+
+      $.getAchievement = function(achievementName) {
+        if (!achievementName) {
+          console.log('Achievement name not provided.');
+          return false;
+        }
+
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        return $.greenworks.getAchievement(achievementName, $._getAchievementSuccess, $._getAchievementError);        
+      };
+
+      $.clearAchievement = function(achievementName) {
+        if (!achievementName) {
+          console.log('Achievement name not provided.');
+          return false;
+        }
+
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        $.greenworks.clearAchievement(achievementName, $._clearAchievementSuccess, $._clearAchievementError);                
+      };
+
+      $.getNumberOfAchievements = function() {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        return $.greenworks.getNumberOfAchievements();
+      };
+
+      $.activateGameOverlay = function(option) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        $.greenworks.activateGameOverlay(option);
+      };
+
+      $.isGameOverlayEnabled = function() {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        return $.greenworks.isGameOverlayEnabled();
+      };
+
+      $.activateGameOverlayToWebPage = function(url) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        $.greenworks.activateGameOverlayToWebPage(url);
+      };
+
+      $.isSubscribedApp = function(appId) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        return $.greenworks.isSubscribedApp(appId);
+      };
+
+      $.getDLCCount = function() {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return 0;
+        }
+        
+        return $.greenworks.getDLCCount();
+      };
+
+      $.isDLCInstalled = function(dlcAppId) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        return $.greenworks.isDLCInstalled(dlcAppId);
+      };
+
+      $.installDLC = function(dlcAppId) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        $.greenworks.installDLC(dlcAppId);
+      };
+
+      $.uninstallDLC = function(dlcAppId) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        $.greenworks.uninstallDLC(dlcAppId);
+      };
+
+      $.getStatInt = function(name) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return 0;
+        }
+        
+        return $.greenworks.getStatInt(name);
+      };
+
+      $.getStatFloat = function(name) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return 0;
+        }
+        
+        return $.greenworks.getStatFloat(name);
+      };
+
+      $.setStat = function(name, value) {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        return $.greenworks.setStat(name, value);
+      };
+
+      $.storeStats = function() {
+        if (!$.isSteamRunning()) {
+          console.log('Steam isn\'t running');
+          return false;
+        }
+        
+        return $.greenworks.setStat($._storeStatsSuccess, $._storeStatsError);
       };
 
       $.getFriendCount = function() {
@@ -120,4 +343,4 @@ Hudell.OrangeGreenworks = Hudell.OrangeGreenworks || {};
 })(Hudell.OrangeGreenworks);
 
 OrangeGreenworks = Hudell.OrangeGreenworks;
-Imported.OrangeGreenworks = 1.1;
+Imported.OrangeGreenworks = 1.2;
